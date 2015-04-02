f9d75d3d4483afa3bee2d7f90d232f5db5b9cc4c
smpatric <patrick.cody.stewart@gmail.com>
2015-04-02 01:18:00 +0000

    Added admin User Creation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,8 +12,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+      redirect_to admin_users_path, notice: "Thanks for adding, #{@user.firstname}!"
     else
       render :new
     end
